3ee1738d9ea0d8df04bf455ca83e1a8ff7234022
PuuGoo <puugoo002@gmail.com>
2025-01-17 20:05:10 +0700

    V0

---
+++ index.js
@@ -0,0 +1,121 @@
+// Khai báo các gói thư viện
+import express from "express"; // Framework dùng để xây dựng ứng dụng web và API
+import bodyParser from "body-parser"; // Middleware để xử lý dữ liệu từ body của request(JSON hoặc URL-encoded)
+import sql from "mssql"; // Thư viện dùng để kết nối và tương tác với cơ sở dữ liệu MySQL
+import axios from "axios"; // Thư viện để thực hiện các yêu cầu HTTP, như gọi API từ Bing
+import { JSDOM } from "jsdom";
+import { fileURLToPath } from "url"; // Import fileURLToPath
+import { dirname } from "path"; // Import dirname
+import path from "path";
+import session from "express-session"; // To manage sessions
+import dotenv from "dotenv"; // To manage sessions
+dotenv.config();
+
+// Get the directory name from import.meta.url
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Khởi tạo ứng dụng
+const app = express(); // App biến đại diện cho ứng dụng Express
+app.use(bodyParser.json()); // Middleware giúp xử lý các request với dữ liệu JSON
+app.use(bodyParser.urlencoded({ extended: true })); // Middleware xử lý dữ liệu URL-encoded từ các form HTML
+app.use(express.static(path.join(__dirname, "public")));
+// Set up express session
+app.use(
+  session({
+    secret: "hotel_search_digi", // A secret key to sign the session ID cookie
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }, // 'secure: false' for non-HTTPS environments
+  })
+);
+
+const dom = new JSDOM(
+  `<!DOCTYPE html><html><body><button id="searchButton">Search</button></body></html>`
+);
+const document = dom.window.document;
+
+// Cấu hình Azure SQL Database
+const dbConfig = {
+  user: "puugoo", // Tên đăng nhập vào Azure SQL Database
+  password: "1303Puu!", // Mật khẩu đăng nhập vào Azure SQL Database
+  server: "digi-hotel.database.windows.net", // Tên máy chủ của Azure SQL Database
+  database: "hotel", // Tên cơ sở dữ liệu cần kết nối
+  option: {
+    encrypt: true, // Yêu cầu mã hóa kết nối (SSL) khi kết nối tới Azure SQL Database
+    trustServerCertificate: false, // Không tin cậy chứng chỉ của máy chủ nếu không sử dụng chứng chỉ SSL đáng tin cậy
+  },
+};
+
+// Kết nối đến Azure SQL Database
+let pool; // Khai báo biến pool để quản lý kết nối cơ sở dữ liệu, giúp tái sử dụng kết nối
+
+// Hàm kết nối tới Azure SQL Database
+async function connectToDatabase() {
+  try {
+    pool = await sql.connect(dbConfig); // Kết nối tới cơ sở dữ liệu với cấu hình đã định nghĩa
+    console.log("Đã kết nối đến Azure SQL Database"); // In thông báo khi kết nối thành công
+  } catch (error) {
+    console.error("Không thể kết nối đến Azure SQL Database:", error); // In thông báo lỗi nếu kết nối không thành công
+  }
+}
+
+connectToDatabase(); // Gọi hàm kết nối đến cơ sở dữ liệu
+
+// Middleware to check if the user is logged in
+function checkAuthenticated(req, res, next) {
+  if (req.session.isAuthenticated) {
+    return next();
+    // return next(); // If user is authenticated, proceed to the next middleware or route handler
+  } else {
+    res.redirect("/"); // If not authenticated, redirect to the login page
+  }
+}
+
+// Định tuyến cho trang đăng nhập
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "login.html"));
+});
+app.get("/hotelSearch", checkAuthenticated, (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "hotelSearch.html"));
+});
+
+// Xử lý yêu cầu đăng nhập
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body; // Lấy thông tin đăng nhập từ body của yêu cầu
+
+  try {
+    // Truy vấn để kiểm tra xem người dùng có tồn tại trong cơ sở dũ liệu với username và password đã nhập
+    const query =
+      "SELECT * FROM users WHERE username = @username and password = @password"; // Dấu @ để đánh dấu tham số, ví dụ @username và @password là các biến tham số được sử dụng thay thế thay vì giá trị cụ thể
+    const result = await pool
+      .request() // Tạo yêu cầu SQL
+      .input("username", sql.VarChar, username) // Thêm tham số 'username' vào câu lệnh SQL
+      .input("password", sql.VarChar, password) // Thêm tham số 'password' vào câu lệnh SQL
+      .query(query); // Thực thi câu lệnh SQL
+
+    // Nếu tìm thấy người dùng trong cơ sở dữ liệu
+    if (result.recordset.length > 0) {
+      req.session.isAuthenticated = true; // Mark user as authenticated
+      res.redirect("/hotelSearch");
+    } else {
+      res.status(401).send("Sai tên đăng nhập hoặc mật khẩu");
+    }
+  } catch (error) {
+    console.error("Lỗi khi truy vấn cơ sở dữ liệu:", error); // In lỗi nếu có sự cố truy vấn cơ sở dữ liệu
+    res.status(500).send("Lỗi máy chủ."); // Trả về mã lỗi 500 (Internal Server Error) nếu gặp lỗi
+  }
+});
+// Route for logging out (to clear the session)
+app.get("/logout", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      return res.status(500).send("Lỗi khi đăng xuất.");
+    }
+    res.redirect("/"); // Redirect to login page after logout
+  });
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server đang chạy tại http://localhost:${process.env.PORT}`);
+});
---
+++ public/hotelSearch.js
@@ -0,0 +1,176 @@
+// Đảm bảo rằng script chỉ chạy khi DOM đã tải xong
+document.addEventListener("DOMContentLoaded", function () {
+  // Phần Javascript thao tác trên trình duyệt (client-side)
+  document
+    .getElementById("searchButton")
+    .addEventListener("click", async () => {
+      // Kiểm tra người dùng có chọn file Excel hay không
+      const fileInput = document.getElementById("fileInput");
+      if (fileInput.files.length === 0) {
+        alert("Vui lòng chọn một file Excel!");
+        return;
+      }
+
+      // Đọc dữ liệu từ file Excel
+      const file = fileInput.files[0];
+      const reader = new FileReader(); // Tạo một FileReader để đọc nội dung file Excel.
+
+      // Cài đặt một số thông số từ BING SEARCH
+      const subscriptionKey = document.getElementById("subscriptionKey").value;
+      console.log(subscriptionKey);
+
+      const endpoint = "https://api.bing.microsoft.com/v7.0/search";
+
+      // Sau khi đọc file Excel hoàn tất ta dùng sự kiện onload xử lý dữ liệu data.
+      reader.onload = async (e) => {
+        const data = new Uint8Array(e.target.result); // Chuyển dữ liệu file thành mãng nhị phân(Unit8Array).
+        const workbook = XLSX.read(data, { type: "array" }); // Đọc toàn bộ file Excel
+        const sheetName = workbook.SheetNames[0]; // Lấy tên của sheet đầu tiên trong file Excel
+        const sheet = workbook.Sheets[sheetName]; // Đọc dữ liệu của sheet đầu tiên trong file Excel
+        const jsonData = XLSX.utils.sheet_to_json(sheet, { header: 1 }); // Theo mặc định sheet_to_json sẽ lấy dòng đầu tiên và sự dụng giá trị như key cho tất cả các dòng còn lại giống mãng kết hợp. Nếu lựa chọn thuộc tính {header: 1} thì nó sẽ xuất thành một mãng các giá trị theo từng dòng file Excel.
+
+        jsonData.shift(); // Bỏ dòng tiêu đề tức dòng đầu tiên
+
+        const results = []; // Tạo một mãng lưu trữ kết quả tìm kiếm được
+        let order = 1; // Biến lưu số thứ tự khách sạn từ file
+
+        // Duyệt qua từng dòng trong file Excel
+        for (let row of jsonData) {
+          let [hotelName, hotelAddress] = row; // Phá hủy một mãng
+          // Nếu dòng dữ liệu trống thì bỏ qua không xử lý
+          if (!hotelName || !hotelAddress) {
+            continue;
+          }
+          hotelName = hotelName.replace(/[^\x00-\x7F]/g, "");
+          const hotelNameArray = hotelName
+            .split(" ")
+            .map((part) =>
+              part
+                .replace(",", "")
+                .replace("(", "")
+                .replace(")", "")
+                .toLowerCase()
+            );
+          console.log(hotelNameArray);
+
+          const query = `${hotelName} ${hotelAddress}`; // Điều kiện tìm kiếm
+          // const searchUrl = `https://www.google.com/search?q=${encodeURIComponent(
+          //   query
+          // )}`; //encodeURIComponent mã hóa 1 chuỗi nhằm đảm bảo chuỗi được truyền an toàn qua URL, và mã hóa toàn bộ ký tự đặc biệt &, ?, =, /, :&, ?, =, /, :, bên cạnh đó encodeURI thì không mã hóa các ký tự đặc biệt &, ?, =, /, :
+          const searchURL = `${endpoint}?q=${encodeURIComponent(
+            query
+          )}&textDecorations=true&textFormat=HTML`;
+
+          let matchedLink = "Không tìm thấy link"; // Giá trị mặc định khi không tìm thấy link
+
+          // Thực hiện tìm kiếm qua Algolia
+          try {
+            const response = await axios.get(searchURL, {
+              headers: {
+                "Ocp-Apim-Subscription-Key": subscriptionKey, // Thêm API Key vào header, và Ocp-Apim-Subscription-Key: là tham số khóa cố định không đổi tên được
+              },
+            });
+            console.log(response);
+
+            // Lấy kết quả từ Bing API
+            const resultsFromBing = response.data.webPages.value;
+
+            if (resultsFromBing && resultsFromBing.length > 0) {
+              const resultsFromBingArray = [];
+              // Lặp qua các kết quả tìm kiếm từ Bing
+              for (let result of resultsFromBing) {
+                const pageTitle = result.name.toLowerCase(); // Tiêu đề của trang
+                // const pageSnippet = result.snippet.toLowerCase(); // Mô tả ngắn gọn của trang
+                const pageUrl = result.url;
+
+                // So sánh tên khách sạn với tiêu đề của trang web
+                const isMatch = isHotelNameInPage(hotelNameArray, pageTitle);
+                if (isMatch.status) {
+                  resultsFromBingArray.push({
+                    percentage: isMatch.percentage,
+                    matchedLink: pageUrl,
+                  });
+                }
+              }
+              const maxPercentageResult = resultsFromBingArray.reduce(
+                (max, item) => {
+                  return item.percentage > max.percentage ? item : max;
+                },
+                { percentage: -Infinity }
+              );
+              matchedLink = maxPercentageResult.matchedLink;
+            }
+          } catch (error) {
+            console.log("Lỗi khi tìm kiếm:", error);
+          }
+          console.log(matchedLink);
+
+          // Thêm số thứ tự vào kết quả , nếu không có link thì vẫn trả về kết quả với chữ "Không tìm thấy link"
+          results.push({
+            order: order++, // Tăng số thứ tự
+            hotelName,
+            hotelAddress,
+            matchedLink,
+          });
+        }
+
+        // Xuất kết quả ra file CSV
+        if (results.length > 0) {
+          downloadCSV(results);
+        } else {
+          alert("Không tìm thấy kết quả nào khớp với tên khách sạn.");
+        }
+      };
+
+      reader.readAsArrayBuffer(file);
+    });
+  document.getElementById("logoutButton").addEventListener("click", () => {
+    window.location.href = "/logout"; // Điều hướng người dùng về trang đăng nhập
+  });
+});
+
+// Hàm xuất ra file CSV
+function downloadCSV(results) {
+  // Tạo nội dung cho file CSV, với dòng đầu là tiêu đề, và các dòng còn lại là nội dung
+  const csvContent =
+    "Order, Hotel Name, Hotel Address, Matched Link\n" +
+    results
+      .map(
+        (row) =>
+          `"${row.order}","${row.hotelName}","${row.hotelAddress}","${row.matchedLink}"`
+      )
+      .join("\n");
+  // Tạo file blob
+  const blob = new Blob([csvContent], { type: "text/csv;charset=utf-8" });
+  // Tạo một liên kết ẩn để tải file
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(blob);
+  link.download = "hotel_search_results.csv";
+  link.style.display = "none";
+
+  document.body.appendChild(link);
+  link.click(); // Kích hoạt tải file tự động
+  document.body.removeChild(link);
+}
+// Hàm kiểm tra tên khách sạn có nằm trong tiêu đề trang hay không
+function isHotelNameInPage(hotelNameArray, pageTitle, pageSnippet) {
+  let matchCount = 0; // Đếm số phân tử trong hotelNameArray khớp với pageTitle
+
+  // Duyệt qua từng phần tử trong mãng hotelNameArray
+  for (let i = 0; i < hotelNameArray.length; i++) {
+    const part = hotelNameArray[i];
+    // So sánh với tiêu đề trang
+    if (pageTitle.includes(part)) {
+      matchCount++; // Nếu phần tử khớp tăng biến đếm
+    }
+  }
+
+  // Kiểm tra nếu số phần tử khớp >= 50% tổng số phần tử trong hotelNameArray
+  const matchPercentage = (matchCount / hotelNameArray.length) * 100;
+  if (matchPercentage >= 50) {
+    return {
+      status: true,
+      percentage: matchPercentage,
+    };
+  }
+}
